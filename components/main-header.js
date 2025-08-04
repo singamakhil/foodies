fbdff78cfffcfc699e433952f74191065ed6fe3d
Singamakhil <singamakhilkumar.com>
2025-08-04 20:15:56 +0530

    created saparate navigation component for rendering client side component

--- components/main-header.js
+++ components/main-header.js
@@ -4,6 +4,7 @@ import logoImg from "@/assets/logo.png";
 import classes from "@/components/main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./NavLink";
 
 export default function MainHeader() {
   return (
@@ -17,10 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Meals Community</Link>
+              <NavLink href="/community">Meals Community</NavLink>
             </li>
           </ul>
         </nav>
