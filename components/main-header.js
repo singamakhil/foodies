397f6e20604987874543e2d2b740dacc8dd99a7d
Singamakhil <singamakhilkumar.com>
2025-08-04 19:54:53 +0530

    added communinity UI page

--- components/main-header.js
+++ components/main-header.js
@@ -3,24 +3,28 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "@/components/main-header.module.css";
 import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <Image src={logoImg} alt="a plate with food" priority />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Meals Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="a plate with food" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Meals Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
