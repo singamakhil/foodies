2e68a290147227b464d78aad772d65ad81dae1ea
Singamakhil <singamakhilkumar.com>
2025-08-05 17:49:48 +0530

    add dummy meals db and displaying meals

--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,8 +1,9 @@
 import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
           <MealItem {...meal} />
