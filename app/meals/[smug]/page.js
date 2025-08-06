3bd6415d07ce191ca0353a7654057a89fab4ca22
Singamakhil <singamakhilkumar.com>
2025-08-06 22:48:16 +0530

    added meals detail page and not found feedback ui

--- app/meals/[smug]/page.js
+++
@@ -1,8 +0,0 @@
-export default async function MealDetailsPage({ params }) {
-  return (
-    <div>
-      <h1>Meal</h1>
-      <p>your meal is {await params.smug}</p>
-    </div>
-  );
-}
---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal not found</h1>
+      <p>Unfortunetly, we could not find requested page or meal</p>
+    </main>
+  );
+}
