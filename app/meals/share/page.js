1e99b5be5f8d4042cb3543a40b1a084f1b8c4a9e
Singamakhil <singamakhilkumar.com>
2025-08-11 23:14:47 +0530

    added server input validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,13 @@
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { ShareMeal } from "@/lib/action";
 import MealFormSubmit from "@/components/meals/meals-form-submit";
+import { useActionState } from "react";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(ShareMeal, { errors: {} });
+
   return (
     <>
       <header className={classes.header}>
@@ -12,36 +16,61 @@ export default function ShareMealPage() {
         </h1>
         <p>Or any other meal you feel needs sharing!</p>
       </header>
+
       <main className={classes.main}>
-        <form className={classes.form} action={ShareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
-              <input type="text" id="name" name="name" required />
+              <input type="text" id="name" name="name" />
+              {state.errors?.creator && (
+                <span className={classes.error}>{state.errors.creator}</span>
+              )}
             </p>
             <p>
               <label htmlFor="email">Your email</label>
-              <input type="email" id="email" name="email" required />
+              <input type="email" id="email" name="email" />
+              {state.errors?.creator_email && (
+                <span className={classes.error}>
+                  {state.errors.creator_email}
+                </span>
+              )}
             </p>
           </div>
+
           <p>
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" required />
+            <input type="text" id="title" name="title" />
+            {state.errors?.title && (
+              <span className={classes.error}>{state.errors.title}</span>
+            )}
           </p>
+
           <p>
             <label htmlFor="summary">Short Summary</label>
-            <input type="text" id="summary" name="summary" required />
+            <input type="text" id="summary" name="summary" />
+            {state.errors?.summary && (
+              <span className={classes.error}>{state.errors.summary}</span>
+            )}
           </p>
+
           <p>
             <label htmlFor="instructions">Instructions</label>
             <textarea
               id="instructions"
               name="instructions"
               rows="10"
-              required
             ></textarea>
+            {state.errors?.instructions && (
+              <span className={classes.error}>{state.errors.instructions}</span>
+            )}
           </p>
+
           <ImagePicker label="Your image" name="image" />
+          {state.errors?.image && (
+            <span className={classes.error}>{state.errors.image}</span>
+          )}
+
           <p className={classes.actions}>
             <MealFormSubmit />
           </p>
